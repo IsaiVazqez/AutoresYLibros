01ba9f7c31432af18473fe98a4e1ce434f7b58de
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-08-18 22:43:17 -0500

    You can make comments and the database will save who make the comment with the usuarioID

--- WebApiAutoresyLibros/Controllers/ComentariosController.cs
+++ WebApiAutoresyLibros/Controllers/ComentariosController.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApiAutoresyLibros.Dtoos;
@@ -12,11 +15,13 @@ namespace WebApiAutoresyLibros.Controllers
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public ComentariosController(ApplicationDbContext context, IMapper mapper)
+        public ComentariosController(ApplicationDbContext context, IMapper mapper, UserManager<IdentityUser> userManager)
         {
             this.context = context;
             this.mapper = mapper;
+            this.userManager = userManager;
         }
         [HttpGet]
         public async Task<ActionResult<List<ComentarioDTO>>> Get(int libroId)
@@ -46,8 +51,17 @@ namespace WebApiAutoresyLibros.Controllers
         }
 
         [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<ActionResult> Post(int libroId, ComentarioCreacionDTO comentarioCreacionDto)
         {
+            var emailClaim = HttpContext.User.Claims.Where(claim => claim.Type == "email").FirstOrDefault();
+
+            var email = emailClaim.Value;
+
+            var usuario = await userManager.FindByEmailAsync(email);
+
+            var usuarioId = usuario.Id;
+
             var existeLibro = await context.Libros.AnyAsync(libroDB => libroDB.Id == libroId);
 
             if(!existeLibro)
@@ -56,12 +70,18 @@ namespace WebApiAutoresyLibros.Controllers
             }
 
             var comentario = mapper.Map<Comentario>(comentarioCreacionDto);
+
             comentario.LibroId = libroId;
+
+            comentario.UsuarioId = usuarioId;
+
             context.Add(comentario);
+
             await context.SaveChangesAsync();
 
             var comentarioDTO = mapper.Map<ComentarioDTO>(comentario);
-            return CreatedAtRoute("ObtenerComentario", new {id = comentario.Id}, comentarioDTO);
+
+            return CreatedAtRoute("ObtenerComentario", new {id = comentario.Id, libroId = libroId}, comentarioDTO);
         }
 
         [HttpPut]
@@ -82,10 +102,15 @@ namespace WebApiAutoresyLibros.Controllers
             }
 
             var comentario = mapper.Map<Comentario>(comentarioCreacionDTO);
+
             comentario.Id = id;
+
             comentario.LibroId = libroId;
+
             context.Update(comentario);
+
             await context.SaveChangesAsync();
+
             return NoContent();
         }
     }
--- WebApiAutoresyLibros/Entities/Comentario.cs
+++ WebApiAutoresyLibros/Entities/Comentario.cs
@@ -1,4 +1,5 @@
-﻿using WebApiAutoresyLibros.Entitys;
+﻿using Microsoft.AspNetCore.Identity;
+using WebApiAutoresyLibros.Entitys;
 
 namespace WebApiAutoresyLibros.Entities
 {
@@ -7,7 +8,9 @@ namespace WebApiAutoresyLibros.Entities
         public int Id { get; set; }
         public string Contenido { get; set; }
         public int LibroId { get; set; }
+        public Libro Libro { get; set; }
 
-        public Libro Libro { get; set; } 
+        public string UsuarioId { get; set; }
+        public IdentityUser Usuario { get; set; }
     }
 }
