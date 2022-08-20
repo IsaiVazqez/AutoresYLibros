ad7fb67ac75dae90606e0d73829890d62c342aa7
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-08-20 18:19:17 -0500

    You can make unit test to validate proccess for the apiii

--- WebApiAutoresyLibros.Tests/PruebasUnitarias/RouteControllerTest.cs
+++ WebApiAutoresyLibros.Tests/PruebasUnitarias/RouteControllerTest.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using WebApiAutoresyLibros.Controllers;
@@ -41,6 +44,38 @@ namespace WebApiAutoresyLibros.Tests.PruebasUnitarias
             authorizationSerice.Resultado = AuthorizationResult.Failed();
 
             var routeController = new RouteController(authorizationSerice);
+
+            routeController.Url = new URLHelperMock();
+            //Ejecución
+
+            var resultado = await routeController.Get();
+
+            //Verificación
+            Assert.AreEqual(2, resultado.Value.Count());
+
+        }
+
+        [TestMethod]
+        public async Task SiUsuarioEsAdmin_Obtenemos2Links_UsandoMMock()
+        {
+            //Preparación
+            var mockAuthorizationService = new Mock<IAuthorizationService>();
+
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<object>(),
+                It.IsAny<IEnumerable<IAuthorizationRequirement>>())).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<object>(),
+                It.IsAny<string>())).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            var mockURLHelper = new Mock<IUrlHelper>();
+
+            mockURLHelper.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns(string.Empty);
+
+            var routeController = new RouteController(mockAuthorizationService.Object);
 
             routeController.Url = new URLHelperMock();
             //Ejecución
